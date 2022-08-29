1816d75fa3f5c203810eedd229cfa09992d79144
Sandro <sandrocarioli43@gmail.com>
2022-08-29 20:52:45 -0300

    Agregado tailwind para aprender a usarlo correctamente/agregados los components/ NavBar Como és requerido

--- coder-house/src/components/NavBar.jsx
+++ coder-house/src/components/NavBar.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import logo from '../media/enoteca.png'
 
-const Header = () => {
-    
+const NavBar = () => {
+    return(
+
+            <div className="p-5 bg-white shadow md:flex md:items-center md:justify-between">
+                <div className="flex justify-between items-center">
+                    <span className="text-2xl font-[Poppins] cursor-pointer">
+                        <img className="h-10 mx-5 inline" src={ logo } alt="Enoteca, cultura en vinos" />
+                    </span>
+                    
+                </div>
+
+                <ul className="md:flex md:items-center">
+                    <li className="mx-4">
+                        <a href="#" className="text-cl hover:text-red-900 duration-500">INICIO</a>
+                    </li>
+                    <li className="mx-4">
+                        <a href="#" className="text-cl hover:text-red-900 duration-500">VINOS</a>
+                    </li>
+                    <li className="mx-4">
+                        <a href="#" className="text-cl hover:text-red-900 duration-500">LICORES Y BREBAJES </a>
+                    </li>
+                    <li className="mx-4">
+                        <a href="#" className="text-cl hover:text-red-900 duration-500">Nosotros</a>
+                    </li>
+                </ul>
+            </div>
+    )
 }
+
+export default NavBar;
